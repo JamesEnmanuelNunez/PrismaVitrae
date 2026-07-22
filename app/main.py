581c392b1583d81ed13447f8b54be0f445dcd969
James Enmanuel Nunez <jamescorniel@gmail.com>
2026-07-22 10:22:29 -0400

    fix: add netlify url to CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -20,7 +20,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "https://prismavitae.netlify.app"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://localhost:5173",
+        "https://prismavitae.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
